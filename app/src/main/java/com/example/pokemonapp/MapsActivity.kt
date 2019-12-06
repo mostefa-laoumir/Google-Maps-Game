25451e7ee28c76c8584d92eb5843b45de4f44547
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-12-06 23:14:09 +0100

    permission getting

--- app/src/main/java/com/example/pokemonapp/MapsActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pokemonapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -23,21 +25,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    fun checkPermision(){
+        if(Build.VERSION.SDK_INT>=23){
+
+        }
+    }
+    fun GetUserLocation(){
+        //get location
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("YOO")
+            .snippet("around the world")
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.daft))
+
+
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,14f))
     }
 }
