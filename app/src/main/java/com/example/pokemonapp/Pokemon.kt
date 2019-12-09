065e95a78e2063b4552da7ef77665eccd8dd93a8
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-12-09 19:42:53 +0100

    pokemon constructor

--- app/src/main/java/com/example/pokemonapp/Pokemon.kt
+++ app/src/main/java/com/example/pokemonapp/Pokemon.kt
@@ -1,5 +1,22 @@
 package com.example.pokemonapp
 
 class Pokemon{
-    
+    var name:String? =null
+    var des:String? =null
+    var image:Int? =null
+    var power:Double? =null
+    var lat:Double? =null
+    var lng:Double? =null
+    var cought:Boolean? = null
+
+    constructor(name:String,des:String,image:Int,power:Double,lat:Double,lng:Double){
+        this.name = name
+        this.des = des
+        this.image = image
+        this.power = power
+        this.lat = lat
+        this.lng = lng
+        this.cought = false
+    }
+
 }
