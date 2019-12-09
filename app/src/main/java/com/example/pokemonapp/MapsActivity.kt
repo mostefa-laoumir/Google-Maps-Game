abebc7f39951bcd76a7acb089da2738001d45dc3
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-12-09 22:25:22 +0100

    final app

--- app/src/main/java/com/example/pokemonapp/MapsActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MapsActivity.kt
@@ -6,10 +6,13 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.support.v4.app.FragmentActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentActivity
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,9 +22,12 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : FragmentActivity(), OnMapReadyCallback  {
 
-    private lateinit var mMap: GoogleMap
+    //WORK WITH USER LOCATION
+
+
+    private var mMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,131 +36,203 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        checkPermision()
-        loadPoke()
+
+
+        checkPermmison()
+        LoadPockemon()
     }
 
-var ACCESSLOCATION = 123
-    fun checkPermision(){
+    var ACCESSLOCATION=123
+    fun checkPermmison(){
+
         if(Build.VERSION.SDK_INT>=23){
-            if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_DENIED){
+
+            if(ActivityCompat.
+                    checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
                 requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),ACCESSLOCATION)
                 return
             }
         }
-        getUserLocation()
+
+        GetUserLocation()
     }
 
+    fun GetUserLocation(){
+        Toast.makeText(this,"User location access on",Toast.LENGTH_LONG).show()
+        //TODO: Will implement later
 
-    fun getUserLocation(){
-        //get location
-            var myLocation = MylocationListener()
-                var locationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3,3f,myLocation)
-                var mythead = myThread()
-                mythead.start()
+        var myLocation= MylocationListener()
 
+        var locationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3,3f,myLocation)
+
+        var mythread=myThread()
+        mythread.start()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
         when(requestCode){
-            ACCESSLOCATION ->{
-                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    getUserLocation()
-                }
-                else{
-                    Toast.makeText(this,"Permission not granted",Toast.LENGTH_LONG).show()
+
+            ACCESSLOCATION->{
+
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    GetUserLocation()
+                }else{
+                    Toast.makeText(this,"We cannot access to your location",Toast.LENGTH_LONG).show()
                 }
             }
         }
+
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
 
     }
 
+    var location:Location?=null
 
-    //get user location
-        var location:Location?=null
+    //Get user location
+
     inner class MylocationListener:LocationListener{
-      constructor(){
 
-        location= Location("start")
-          location!!.longitude=0.0
-          location!!.latitude= 0.0
-      }
 
+        constructor(){
+            location= Location("Start")
+            location!!.longitude=0.0
+            location!!.longitude=0.0
+        }
         override fun onLocationChanged(p0: Location?) {
-                    location=p0
-
+            location=p0
         }
 
-        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-          //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun onProviderEnabled(provider: String?) {
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
             //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
 
-        override fun onProviderDisabled(provider: String?) {
-             //ODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun onProviderEnabled(p0: String?) {
+            // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
 
     }
 
 
+    var oldLocation:Location?=null
     inner class myThread:Thread{
+
         constructor():super(){
-            }
-        override fun run() {
-            while(true){
+            oldLocation= Location("Start")
+            oldLocation!!.longitude=0.0
+            oldLocation!!.longitude=0.0
+        }
+
+        override fun run(){
+
+            while (true){
+
                 try {
-                    runOnUiThread{
+
+                    if(oldLocation!!.distanceTo(location)==0f){
+                        continue
+                    }
+
+                    oldLocation=location
+
+
+                    runOnUiThread {
+
+
                         mMap!!.clear()
-                        val mario = LatLng(location!!.latitude, location!!.longitude)
-                       //show me
+
+                        // show me
+                        val sydney = LatLng(location!!.latitude, location!!.longitude)
                         mMap!!.addMarker(MarkerOptions()
-                            .position(mario)
-                            .title("YOO")
-                            .snippet("around the world")
+                            .position(sydney)
+                            .title("Me")
+                            .snippet(" here is my location")
                             .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
-                        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(mario,14f))
-                        // show pokemons
-                        for(i in 0..pokeList.size-1){
-                            var newPoke = pokeList[i]
-                            if(pokeList[i].cought==false){
-                                val poke = LatLng(pokeList[i].lat!!, pokeList[i].lng!!)
-                                //show me
+                        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14f))
+
+                        // show pockemons
+
+                        for(i in 0..listPockemons.size-1){
+
+                            var newPockemon=listPockemons[i]
+
+                            if(newPockemon.IsCatch==false){
+
+                                val pockemonLoc = LatLng(newPockemon.location!!.latitude, newPockemon.location!!.longitude)
                                 mMap!!.addMarker(MarkerOptions()
-                                    .position(poke)
-                                    .title(pokeList[i].name)
-                                    .snippet(pokeList[i].des)
-                                    .icon(BitmapDescriptorFactory.fromResource(pokeList[i].image!!)))
-                                mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(mario,14f))
+                                    .position(pockemonLoc)
+                                    .title(newPockemon.name!!)
+                                    .snippet(newPockemon.des!! +", power:"+ newPockemon!!.power)
+                                    .icon(BitmapDescriptorFactory.fromResource(newPockemon.image!!)))
+
+
+                                if (location!!.distanceTo(newPockemon.location)<2){
+                                    newPockemon.IsCatch=true
+                                    listPockemons[i]=newPockemon
+                                    playerPower+=newPockemon.power!!
+                                    Toast.makeText(applicationContext,
+                                        "You catch new pockemon your new pwoer is " + playerPower,
+                                        Toast.LENGTH_LONG).show()
+
+                                }
+
                             }
                         }
 
+
+
+
+
                     }
 
-                }catch (ex:Exception){
+                    Thread.sleep(1000)
 
-                }
+                }catch (ex:Exception){}
+
+
             }
+
         }
+
     }
-    var pokeList = ArrayList<Pokemon>()
-    fun loadPoke(){
-        pokeList.add(Pokemon("charmander", "I am in Japan", R.drawable.charmander, 55.0,37.7789994893035, -122.401846647263))
-        pokeList.add(Pokemon("Bulbasaur", "I am in USA", R.drawable.bulbasaur, 90.5,37.7949568502667, -122.410494089127))
-        pokeList.add(Pokemon("Squirtle", "I am in ALGERIA", R.drawable.squirtle, 331.0,37.7816621152613, -122.41225361824))
+
+
+    var playerPower=0.0
+    var listPockemons=ArrayList<Pockemon>()
+
+    fun  LoadPockemon(){
+
+
+        listPockemons.add(Pockemon(R.drawable.charmander,
+            "Charmander", "Charmander living in japan", 55.0, 37.7789994893035, -122.401846647263))
+        listPockemons.add(Pockemon(R.drawable.bulbasaur,
+            "Bulbasaur", "Bulbasaur living in usa", 90.5, 37.7949568502667, -122.410494089127))
+        listPockemons.add(Pockemon(R.drawable.squirtle,
+            "Squirtle", "Squirtle living in iraq", 33.5, 37.7816621152613, -122.41225361824))
 
     }
 
--- app/src/main/java/com/example/pokemonapp/Pokemon.kt
+++ app/src/main/java/com/example/pokemonapp/Pokemon.kt
@@ -1,22 +1,26 @@
 package com.example.pokemonapp
 
-class Pokemon{
-    var name:String? =null
-    var des:String? =null
-    var image:Int? =null
-    var power:Double? =null
-    var lat:Double? =null
-    var lng:Double? =null
-    var cought:Boolean? = null
 
-    constructor(name:String,des:String,image:Int,power:Double,lat:Double,lng:Double){
-        this.name = name
-        this.des = des
-        this.image = image
-        this.power = power
-        this.lat = lat
-        this.lng = lng
-        this.cought = false
+
+import android.location.Location
+
+class  Pockemon{
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+    var power:Double?=null
+    var location:Location?=null
+    var IsCatch:Boolean?=false
+    constructor(image:Int,name:String,des:String,power:Double,lat:Double,log:Double){
+        this.name=name
+        this.des=des
+        this.image=image
+        this.power=power
+        this.location= Location(name)
+        this.location!!.latitude=lat
+        this.location!!.longitude=log
+        this.IsCatch=false
     }
 
 }
+
